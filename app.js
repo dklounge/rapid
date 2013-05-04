d8112a7f27d8ba7d98b2a5c192d4ecf49aeb52f8
dklounge <daviddarden11@gmail.com>
2013-05-04 16:12:11 -0700

    updated app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var parseApplicationID = "nPohS4DhxXsHWMA9SuoQxFCrzxHJDJngATtJOA52";
   var parseJavaScriptKey="yWzHT4Ix9pfKokjyr59IiBmwP6DovyoIMoCpAEiQ";
 
-  Parse.initialize(parseApplicationID, parseJavaScriptKey)  ;
+  Parse.initialize(parseApplicationID, parseJavaScriptKey);
   var Test = Parse.Object.extend("Test");
   var test = new Test();
   test.save({
