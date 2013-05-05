fe69b17946f91ac54748b064ecf5fe1910e6a5ce
dklounge <daviddarden11@gmail.com>
2013-05-04 17:13:16 -0700

    added new logic to app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,72 @@
+// obtain keys from Parse.com dashboard
+
+var parseID = "nPohS4DhxXsHWMA9SuoQxFCrzxHJDJngATtJOA52";
+var parseRestKey = "yd9TPmCIayqrStv7O45PzGKqu9a55zLqzLmcNu8V";
+
 $(document).ready(function() {
-  // obtain keys from Parse.com dashboard
-
-  var parseApplicationID = "nPohS4DhxXsHWMA9SuoQxFCrzxHJDJngATtJOA52";
-  var parseJavaScriptKey="yWzHT4Ix9pfKokjyr59IiBmwP6DovyoIMoCpAEiQ";
-
-  Parse.initialize(parseApplicationID, parseJavaScriptKey);
-  var Test = Parse.Object.extend("Test");
-  var test = new Test();
-  test.save({
-    name: "David",
-    text: "hello"}, {
-    success: function(obj) {
-      // alternate would be 
-      // alert("Parse.com object is saved");
-      console.log("Prase.com object is saved " + obj +" "+ obj.name);
-    },
-    error: function(obj) {
-      console.log("Error has occured! Object not saved " + obj);
-    }
+  getMessages();
+  $("#send").click(function(){
+    var username = $('input[name=username]').attr('value');
+    var message = $('input[name=message]').attr('value');
+    console.log(username)
+    console.log('!'')
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/MessageBoard',
+      headers: {
+        'X-Parse-Application-ID': parseID,
+        'X-Parse-REST-API-Key': parseRestKey
+      },
+      contentType: 'application/json',
+      dataType: 'json',
+      processData: false,
+      data: JSON.stringify({
+        'username': username,
+        'message': message
+      }),
+      type: 'POST',
+      succes: function() {
+        console.log('sent');
+        getMessages();
+      },
+      error: function() {
+        console.log('error');
+      }
+    });
   });
 });
+
+function getMessages() {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/MessageBoard',
+    headers: {
+      'X-Parse-Application-ID': parseID,
+      'X-Parse-REST-API-Key': parseRestKey
+    },
+    contentType: 'application/json',
+    dataType: 'json',
+    type: 'GET',
+    
+    succes: function(data) {
+      console.log('get');
+      updateView(data);
+    },
+    error: function() {
+      console.log('error');
+    }
+  });
+}
+
+function updateView(messages) {
+  var table=$('.table tbody');
+  table.html('');
+  $.each(messages.results, function(index, value) {
+    var trEl = 
+      $('<tr><td>'
+        + value.username
+        + '</td><td>'
+        + value.message +
+        '</td></tr>');
+    table.append(trEl);
+  });
+  console.log(messages);
+}
